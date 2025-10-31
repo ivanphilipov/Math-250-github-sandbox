3a9356ddc0ba5be8122dd36c9dd6794ba8826202
Ivan Philipov <i1philipov@gmail.com>
2025-10-31 10:37:32 -0500

    Add normal ghost image

---
+++ githubsetup.R
@@ -0,0 +1,12 @@
+library(usethis)
+# FIrst register for a github account
+
+create_github_token()
+
+
+#To push
+use_github()
+
+#im gonna start a new project first
+
+#Lets make a change or two. Ill add that ghost cartoon
